b042af2a540c7d49cd1567609699c7474c79632c
EBA96 <44510439+EBA96@users.noreply.github.com>
2021-09-25 04:33:00 +0300

    Proje dosyası ekle.

---
+++ HW1-EnesBayramAfsar1/Controllers/PhonebookController.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HW1_EnesBayramAfsar1.Controllers
+{
+    [Route("api/phonebooks")]
+    [ApiController]
+    public class PhonebookController : ControllerBase
+    {
+        private static List<PhonebookModel> PhonebookList = new List<PhonebookModel>()
+        {
+
+            new PhonebookModel() { Id = 1, İsim = "Enes", Soyisim = "Afsar", Cinsiyet = "E", Telefon = "0553-853-18-03" },
+            new PhonebookModel() { Id = 2, İsim = "Deneme", Soyisim = "Deneme", Cinsiyet = "K", Telefon = "0553-853-18-03" },
+            new PhonebookModel() { Id = 3, İsim = "Deneme2", Soyisim = "Deneme2", Cinsiyet = "E", Telefon = "0553-853-18-03" }
+        };
+
+
+        private bool Compare(string? pattern, string property)
+        {
+            if (pattern == null || pattern.Length == 0)
+            {
+                return true;
+            }
+            return property.ToLower().Contains(pattern.ToLower());
+        }
+
+        private bool Compare(int? number, int property)
+        {
+            if (number == null)
+            {
+                return true;
+            }
+            return number == property;
+        }
+        [HttpGet]
+        public IActionResult GetPhonebooks()
+        {
+            return Ok(PhonebookList);
+        }
+        [HttpGet("list")]
+        public IActionResult GetPhonebooksFromQuery([FromQuery] int? id,  string isim, string soyisim, string cinsiyet, string telefon)
+        {
+            var filteredPhonebooks = PhonebookList.Where(x => Compare(id, x.Id) &&  
+              Compare(isim, x.İsim) && Compare(soyisim, x.Soyisim) && Compare(cinsiyet, x.Cinsiyet) && Compare(telefon, x.Telefon));
+            return Ok(filteredPhonebooks);
+        }
+        [HttpPut]
+        public IActionResult UpdatePhonebook([FromBody] PhonebookModel phonebook)
+        {
+            if (ModelState.IsValid)
+            {
+                var upToUpdate = PhonebookList.FirstOrDefault(x => x.Id == phonebook.Id);
+                if (upToUpdate != null)
+                {
+                    PhonebookList.Remove(upToUpdate);
+                    PhonebookList.Add(phonebook);
+                    return Created($"api/phonebooks/{phonebook.Id}", phonebook);
+                }
+                return BadRequest("Kayıt bulunamadı!");
+            }
+            return BadRequest();
+        }
+        [HttpPost]
+        public IActionResult AddPhonebook([FromBody] PhonebookModel phonebook)
+        {
+            if (ModelState.IsValid)
+            {
+                PhonebookList.Add(phonebook);
+                return Created($"api/phonebooks/{phonebook.Id}", phonebook);
+            }
+            return BadRequest();
+        }
+        [HttpDelete]
+        public IActionResult DeletePhonebook([FromQuery] int id)
+        {
+            var upToDelete = PhonebookList.FirstOrDefault(x => x.Id == id);
+            if (upToDelete != null)
+            {
+                PhonebookList.Remove(upToDelete);
+                return Ok();
+            }
+            return NotFound("Kayıt bulunamadı!");
+        }
+
+    }
+}
---
+++ HW1-EnesBayramAfsar1/PhonebookModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+
+namespace HW1_EnesBayramAfsar1
+{
+    public class PhonebookModel
+    {
+        [Required]
+        public int Id { get; set; }
+       
+        [StringLength(30, MinimumLength = 3, ErrorMessage = "Karakter sayısını kontrol ediniz!")]
+        public string İsim { get; set; }
+
+        [StringLength(30, MinimumLength = 3, ErrorMessage = "Karakter sayısını kontrol ediniz!")]
+        public string Soyisim { get; set; }
+
+        [Required]
+        [RegularExpression("(E|K)", ErrorMessage = "Erkek için (E), Kadın için (K) giriniz!")]
+        public string Cinsiyet { get; set; }
+
+        [RegularExpression("^(0(d{3})-(d{3})-(d{2})-(d{2}))$", ErrorMessage = "Telefon numarasını doğru formatta giriniz!")]
+        public string Telefon { get; set; }
+        
+    }
+}
